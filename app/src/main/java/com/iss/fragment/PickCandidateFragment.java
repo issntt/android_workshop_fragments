7eb117d18b4c507cd988d266059f1af68ab0c747
issntt <issntt@nus.edu.sg>
2019-12-05 17:02:11 +0800

    This is a workshop for Fragment. Commit everything.

---
+++ app/src/main/java/com/iss/fragment/PickCandidateFragment.java
@@ -0,0 +1,62 @@
+package com.iss.fragment;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class PickCandidateFragment extends Fragment
+    implements View.OnClickListener {
+
+    PickCandidateFragment.resultInterface callback;
+
+    public PickCandidateFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pickcandidate, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        for (int i=0; i<4; i++) {
+            int id = getResources().getIdentifier("btn" + i,
+                    "id", getActivity().getPackageName());
+            Button btn = view.findViewById(id);
+            btn.setOnClickListener(this);
+        }
+    }
+
+    // Note: this method can be replaced by the onAttachFragment() on the Activity side
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        callback = (resultInterface) context;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button btn = (Button) v;
+        ColorDrawable bkgd = (ColorDrawable) btn.getBackground();
+        int color = bkgd.getColor();
+        String candidate = btn.getText().toString();
+
+        callback.onReturnResults(candidate, color);
+    }
+
+    public void setCallback(PickCandidateFragment.resultInterface callback) {
+        this.callback = callback;
+    }
+
+    public interface resultInterface {
+        void onReturnResults(String name, int color);
+    }
+}
